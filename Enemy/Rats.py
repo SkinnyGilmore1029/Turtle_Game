870dd690b86119afeb275dfb643055dc801c3f4a
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-06-06 20:02:18 -0500

    Rat made and moving trouble with wall collsion tho

--- Enemy/Rats.py
+++ Enemy/Rats.py
@@ -8,11 +8,16 @@ class The_Rats(Bad_guy):
         
         
     def move(self,dt)->None:
-        ...
+        self.rect.y += self.velocity.y * dt
+        self.rect.x -= self.velocity.x * dt
+        if self.rect.y <= 0 or self.rect.y >= 800 - self.h:
+            self.velocity.y *= -1
+        if self.rect.x <= 0 or self.rect.x >= 1200 - self.w:
+            self.velocity.x *= -1
     
     def update(self,dt)->None:
-        #self.move(dt)
-        pass
+        self.move(dt)
+        self.handle_animations()
         
     def draw(self,screen:pygame.Surface)->None:
         screen.blit(self.image,self.rect)
